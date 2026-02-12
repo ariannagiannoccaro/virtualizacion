400ba20d8bb7686f5216d4bd14e90c10b62fd446
ariannagiannoccaro <123045858@upq.edu.mx>
2026-02-12 15:28:18 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cuál es tu nombre?: ")
+edad = input("¿Cuál es tu edad?: ")
+
+print("\n--- INFORMACIÓN RECOPILADA ---")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad}")
+print("-----------------------------\n")
